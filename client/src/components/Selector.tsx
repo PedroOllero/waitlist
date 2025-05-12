c73d41e7f4caa66b8eaa6136a53c8e2d26682679
Pedro Ollero <pedroollero1998@gmail.com>
2025-05-12 10:17:08 +0200

    Enhance Selector component; add state management for selected value and update button to handle selection

--- client/src/components/Selector.tsx
+++ client/src/components/Selector.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 interface SelectorProps{
     value: string;
     children?: string
 }
+
 export const Selector: React.FC <SelectorProps>= ({value, children}) => {
-  return <button className="bg-background-200 rounded-4xl py-2 px-4 text-sm">{children}</button>;
+  const [selected, setSelected] = useState("")
+  const handleSelected = () => {
+      setSelected(value);
+  }
+
+  return <button onToggle={handleSelected} value={value} className="bg-background-200 rounded-4xl py-2 px-4 text-sm">{children}</button>;
 };
