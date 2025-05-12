5edc83ebe3e272d36136dd63495abd2f5079faa4
Pedro Ollero <pedroollero1998@gmail.com>
2025-05-12 10:52:25 +0200

    Enhance Selector component; implement toggle functionality for selection state and update button styling

--- client/src/components/Selector.tsx
+++ client/src/components/Selector.tsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
-
 interface SelectorProps{
     value: string;
     children?: string
 }
 
 export const Selector: React.FC <SelectorProps>= ({value, children}) => {
-  const [selected, setSelected] = useState("")
-  const handleSelected = () => {
-      setSelected(value);
+  const [isSelected, setIsSelected] = useState(false)
+
+  const handleSelected = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    if(!isSelected){
+      setIsSelected(true)
+    }else{
+      setIsSelected(false)
+    }
   }
 
-  return <button onToggle={handleSelected} value={value} className="bg-background-200 rounded-4xl py-2 px-4 text-sm">{children}</button>;
+  console.log("Selecciones", value, isSelected)
+
+  return <button onClick={handleSelected} value={value} className={`rounded-4xl py-2 px-4 text-sm transition-all duration-300 ease-in-out ${isSelected ? "bg-primary-200 " : "bg-background-200 "}`}>{children}</button>;
 };
